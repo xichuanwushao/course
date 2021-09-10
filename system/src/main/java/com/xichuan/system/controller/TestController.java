2fed7cc52e6890f0d2f53afba254e06cd08a401f
xichuanwushao <avatarwx@163.com>
2021-09-10 16:07:09 +0800

    2-3 在gateway增加system路由转发配置 调试成功 1.需要用9001访问的统一用9000端口进行访问 2.http://localhost:9001/system/test http://localhost:9000/system/test

--- system/src/main/java/com/xichuan/system/controller/TestController.java
+++ system/src/main/java/com/xichuan/system/controller/TestController.java
@@ -4,7 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 //@Controller 如果接口返回页面用Controller
 @RestController//@Controller 如果接口返回Json 用RestController
-@RequestMapping("test")
 public class TestController {
     @RequestMapping("test")
     public String test(){
