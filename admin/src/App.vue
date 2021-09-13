5636cb266d431877484595a6c48218b3d21b1ca7
xichuanwushao <avatarwx@163.com>
2021-09-13 16:20:08 +0800

    4-2 修改登录页 去掉多余元素

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -7,10 +7,10 @@
                         <div class="center">
                             <h1>
                                 <i class="ace-icon fa fa-leaf green"></i>
-                                <span class="red">Ace</span>
-                                <span class="white" id="id-text2">Application</span>
+                                <span class="red">西川</span>
+                                <span class="" >控台登陆</span>
                             </h1>
-                            <h4 class="blue" id="id-company-text">&copy; Company Name</h4>
+                            <h4 class="blue" id="id-company-text"></h4>
                         </div>
 
                         <div class="space-6"></div>
@@ -21,7 +21,7 @@
                                     <div class="widget-main">
                                         <h4 class="header blue lighter bigger">
                                             <i class="ace-icon fa fa-coffee green"></i>
-                                            Please Enter Your Information
+                                            请输入用户名和密码
                                         </h4>
 
                                         <div class="space-6"></div>
@@ -30,14 +30,14 @@
                                             <fieldset>
                                                 <label class="block clearfix">
 														<span class="block input-icon input-icon-right">
-															<input type="text" class="form-control" placeholder="Username" />
+															<input type="text" class="form-control" placeholder="用户名" />
 															<i class="ace-icon fa fa-user"></i>
 														</span>
                                                 </label>
 
                                                 <label class="block clearfix">
 														<span class="block input-icon input-icon-right">
-															<input type="password" class="form-control" placeholder="Password" />
+															<input type="password" class="form-control" placeholder="密码" />
 															<i class="ace-icon fa fa-lock"></i>
 														</span>
                                                 </label>
@@ -47,12 +47,12 @@
                                                 <div class="clearfix">
                                                     <label class="inline">
                                                         <input type="checkbox" class="ace" />
-                                                        <span class="lbl"> Remember Me</span>
+                                                        <span class="lbl"> 记住我</span>
                                                     </label>
 
                                                     <button type="button" class="width-35 pull-right btn btn-sm btn-primary">
                                                         <i class="ace-icon fa fa-key"></i>
-                                                        <span class="bigger-110">Login</span>
+                                                        <span class="bigger-110">登录</span>
                                                     </button>
                                                 </div>
 
@@ -60,85 +60,85 @@
                                             </fieldset>
                                         </form>
 
-                                        <div class="social-or-login center">
-                                            <span class="bigger-110">Or Login Using</span>
-                                        </div>
+<!--                                        <div class="social-or-login center">-->
+<!--                                            <span class="bigger-110">Or Login Using</span>-->
+<!--                                        </div>-->
 
-                                        <div class="space-6"></div>
+<!--                                        <div class="space-6"></div>-->
 
-                                        <div class="social-login center">
-                                            <a class="btn btn-primary">
-                                                <i class="ace-icon fa fa-facebook"></i>
-                                            </a>
+<!--                                        <div class="social-login center">-->
+<!--                                            <a class="btn btn-primary">-->
+<!--                                                <i class="ace-icon fa fa-facebook"></i>-->
+<!--                                            </a>-->
 
-                                            <a class="btn btn-info">
-                                                <i class="ace-icon fa fa-twitter"></i>
-                                            </a>
+<!--                                            <a class="btn btn-info">-->
+<!--                                                <i class="ace-icon fa fa-twitter"></i>-->
+<!--                                            </a>-->
 
-                                            <a class="btn btn-danger">
-                                                <i class="ace-icon fa fa-google-plus"></i>
-                                            </a>
-                                        </div>
+<!--                                            <a class="btn btn-danger">-->
+<!--                                                <i class="ace-icon fa fa-google-plus"></i>-->
+<!--                                            </a>-->
+<!--                                        </div>-->
                                     </div><!-- /.widget-main -->
 
-                                    <div class="toolbar clearfix">
-                                        <div>
-                                            <a href="#" data-target="#forgot-box" class="forgot-password-link">
-                                                <i class="ace-icon fa fa-arrow-left"></i>
-                                                I forgot my password
-                                            </a>
-                                        </div>
+<!--                                    <div class="toolbar clearfix">-->
+<!--                                        <div>-->
+<!--                                            <a href="#" data-target="#forgot-box" class="forgot-password-link">-->
+<!--                                                <i class="ace-icon fa fa-arrow-left"></i>-->
+<!--                                                I forgot my password-->
+<!--                                            </a>-->
+<!--                                        </div>-->
 
-                                        <div>
-                                            <a href="#" data-target="#signup-box" class="user-signup-link">
-                                                I want to register
-                                                <i class="ace-icon fa fa-arrow-right"></i>
-                                            </a>
-                                        </div>
-                                    </div>
+<!--                                        <div>-->
+<!--                                            <a href="#" data-target="#signup-box" class="user-signup-link">-->
+<!--                                                I want to register-->
+<!--                                                <i class="ace-icon fa fa-arrow-right"></i>-->
+<!--                                            </a>-->
+<!--                                        </div>-->
+<!--                                    </div>-->
                                 </div><!-- /.widget-body -->
                             </div><!-- /.login-box -->
 
-                            <div id="forgot-box" class="forgot-box widget-box no-border">
-                                <div class="widget-body">
-                                    <div class="widget-main">
-                                        <h4 class="header red lighter bigger">
-                                            <i class="ace-icon fa fa-key"></i>
-                                            Retrieve Password
-                                        </h4>
+<!--                            <div id="forgot-box" class="forgot-box widget-box no-border">-->
+<!--                                <div class="widget-body">-->
+<!--                                    <div class="widget-main">-->
+<!--                                        <h4 class="header red lighter bigger">-->
+<!--                                            <i class="ace-icon fa fa-key"></i>-->
+<!--                                            Retrieve Password-->
+<!--                                        </h4>-->
 
-                                        <div class="space-6"></div>
-                                        <p>
-                                            Enter your email and to receive instructions
-                                        </p>
+<!--                                        <div class="space-6"></div>-->
+<!--                                        <p>-->
+<!--                                            Enter your email and to receive instructions-->
+<!--                                        </p>-->
 
-                                        <form>
-                                            <fieldset>
-                                                <label class="block clearfix">
-														<span class="block input-icon input-icon-right">
-															<input type="email" class="form-control" placeholder="Email" />
-															<i class="ace-icon fa fa-envelope"></i>
-														</span>
-                                                </label>
+<!--                                        <form>-->
+<!--                                            <fieldset>-->
+<!--                                                <label class="block clearfix">-->
+<!--														<span class="block input-icon input-icon-right">-->
+<!--															<input type="email" class="form-control" placeholder="Email" />-->
+<!--															<i class="ace-icon fa fa-envelope"></i>-->
+<!--														</span>-->
+<!--                                                </label>-->
 
-                                                <div class="clearfix">
-                                                    <button type="button" class="width-35 pull-right btn btn-sm btn-danger">
-                                                        <i class="ace-icon fa fa-lightbulb-o"></i>
-                                                        <span class="bigger-110">Send Me!</span>
-                                                    </button>
-                                                </div>
-                                            </fieldset>
-                                        </form>
-                                    </div><!-- /.widget-main -->
+<!--                                                <div class="clearfix">-->
+<!--                                                    <button type="button" class="width-35 pull-right btn btn-sm btn-danger">-->
+<!--                                                        <i class="ace-icon fa fa-lightbulb-o"></i>-->
+<!--                                                        <span class="bigger-110">Send Me!</span>-->
+<!--                                                    </button>-->
+<!--                                                </div>-->
+<!--                                            </fieldset>-->
+<!--                                        </form>-->
+<!--                                    </div>&lt;!&ndash; /.widget-main &ndash;&gt;-->
 
-                                    <div class="toolbar center">
-                                        <a href="#" data-target="#login-box" class="back-to-login-link">
-                                            Back to login
-                                            <i class="ace-icon fa fa-arrow-right"></i>
-                                        </a>
-                                    </div>
-                                </div><!-- /.widget-body -->
-                            </div><!-- /.forgot-box -->
+<!--                                    <div class="toolbar center">-->
+<!--                                        <a href="#" data-target="#login-box" class="back-to-login-link">-->
+<!--                                            Back to login-->
+<!--                                            <i class="ace-icon fa fa-arrow-right"></i>-->
+<!--                                        </a>-->
+<!--                                    </div>-->
+<!--                                </div>&lt;!&ndash; /.widget-body &ndash;&gt;-->
+<!--                            </div>&lt;!&ndash; /.forgot-box &ndash;&gt;-->
 
                             <div id="signup-box" class="signup-box widget-box no-border">
                                 <div class="widget-body">
@@ -217,20 +217,20 @@
                             </div><!-- /.signup-box -->
                         </div><!-- /.position-relative -->
 
-                        <div class="navbar-fixed-top align-right">
-                            <br />
-                            &nbsp;
-                            <a id="btn-login-dark" href="#">Dark</a>
-                            &nbsp;
-                            <span class="blue">/</span>
-                            &nbsp;
-                            <a id="btn-login-blur" href="#">Blur</a>
-                            &nbsp;
-                            <span class="blue">/</span>
-                            &nbsp;
-                            <a id="btn-login-light" href="#">Light</a>
-                            &nbsp; &nbsp; &nbsp;
-                        </div>
+<!--                        <div class="navbar-fixed-top align-right">-->
+<!--                            <br />-->
+<!--                            &nbsp;-->
+<!--                            <a id="btn-login-dark" href="#">Dark</a>-->
+<!--                            &nbsp;-->
+<!--                            <span class="blue">/</span>-->
+<!--                            &nbsp;-->
+<!--                            <a id="btn-login-blur" href="#">Blur</a>-->
+<!--                            &nbsp;-->
+<!--                            <span class="blue">/</span>-->
+<!--                            &nbsp;-->
+<!--                            <a id="btn-login-light" href="#">Light</a>-->
+<!--                            &nbsp; &nbsp; &nbsp;-->
+<!--                        </div>-->
                     </div>
                 </div><!-- /.col -->
             </div><!-- /.row -->
