11fe6651a1e6ecd7c5b7c1fd12a11a5847eceb00
xichuanwushao <avatarwx@163.com>
2021-09-14 17:44:40 +0800

    5-3 前端调用章节列表接口 解决跨域问题 1.跨域配置放在server公共模块中

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -980,9 +980,16 @@
         name: "chapter",
         mounted:function () {
             // this.$parent.activeSidebar("business-chapter-sidebar");
+            let _this = this;
+            _this.list();
         },
         methods:{
-
+            list(){
+                let _this = this;
+                _this.$ajax.get("http://127.0.0.1:9002/business/chapterlist").then((response=>{
+                    console.log("查询章列表结果：",response);
+                }))
+            }
         }
 
     }
---
+++ server/src/main/java/com/xichuan/server/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.xichuan.server.config;
+
+import com.xichuan.server.ServerApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    private static final Logger logger = LoggerFactory.getLogger(ServerApplication.class);
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        logger.warn("server模块......跨域配置已经加载......");
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders(CorsConfiguration.ALL)
+                .allowedMethods(CorsConfiguration.ALL)
+                .allowCredentials(true)
+                .maxAge(3600); // 1小时内不需要再预检（发OPTIONS请求）
+    }
+
+}
