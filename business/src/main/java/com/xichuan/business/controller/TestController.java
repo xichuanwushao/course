c08d467590d4725cdd84ad849dffc911c3e8b595
xichuanwushao <avatarwx@163.com>
2021-09-14 13:47:25 +0800

    5-1 增加business模块 并增加初始启动代码

---
+++ business/src/main/java/com/xichuan/business/controller/TestController.java
@@ -0,0 +1,26 @@
+package com.xichuan.business.controller;
+
+import com.xichuan.server.domain.Test;
+import com.xichuan.server.service.TestService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+//@Controller 如果接口返回页面用Controller
+@RestController//@Controller 如果接口返回Json 用RestController
+public class TestController {
+    @Resource
+    private TestService testService;
+
+    @RequestMapping("test")
+    public String test(){
+        return "success";
+    }
+
+    @RequestMapping("testlist")
+    public List<Test> testlist(){
+       return testService.list();
+    }
+}
