f6895d806a140ac0c6c85ab339918d7df1ee2648
xichuanwushao <avatarwx@163.com>
2021-09-16 16:57:50 +0800

    5-8 修改弹出提示框大气一些 1.使用https://sweetalert2.github.io/#examples的A custom positioned dialog

--- admin/public/static/js/toast.js
+++ admin/public/static/js/toast.js
@@ -1,35 +1,44 @@
-const Toast = Swal.mixin({
-    toast: true,
-    position: 'top-end',
-    showConfirmButton: false,
-    timer: 3000,
-    timerProgressBar: true,
-    didOpen: (toast) => {
-        toast.addEventListener('mouseenter', Swal.stopTimer)
-        toast.addEventListener('mouseleave', Swal.resumeTimer)
-    }
-})
+// const Toast = Swal.mixin({
+//     toast: true,
+//     position: 'top-end',
+//     showConfirmButton: false,
+//     timer: 3000,
+//     timerProgressBar: true,
+//     didOpen: (toast) => {
+//         toast.addEventListener('mouseenter', Swal.stopTimer)
+//         toast.addEventListener('mouseleave', Swal.resumeTimer)
+//     }
+// })
 
 
 
 let toast;
 toast = {
     success:function (message) {
-        Toast.fire({
-            icon:'success',
-            title: message
+        Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: message,
+            showConfirmButton: false,
+            timer: 3000
         })
     },
     error:function () {
-        Toast.fire({
-            icon:'error',
-            title: message
+        Swal.fire({
+            position: 'top-end',
+            icon: 'error',
+            title: message,
+            showConfirmButton: false,
+            timer: 3000
         })
     },
     warning:function () {
-        Toast.fire({
-            icon:'warning',
-            title: message
+        Swal.fire({
+            position: 'top-end',
+            icon: 'warning',
+            title: message,
+            showConfirmButton: false,
+            timer: 3000
         })
     }
 }
