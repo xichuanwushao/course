9ae8df45241f4bb7bec245fd8579acf0b23285d1
xichuanwushao <avatarwx@163.com>
2021-09-17 17:41:11 +0800

    6-2 代码生成器开发 解决乱码问题

--- generator/src/main/java/com/xichuan/generator/util/FreemarkerUtil.java
+++ generator/src/main/java/com/xichuan/generator/util/FreemarkerUtil.java
@@ -5,10 +5,7 @@ import freemarker.template.DefaultObjectWrapper;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.Map;
 
 public class FreemarkerUtil {
@@ -19,14 +16,17 @@ public class FreemarkerUtil {
 
     public static void initConfig(String ftlName) throws IOException {
         Configuration cfg = new Configuration(Configuration.VERSION_2_3_29);
+        cfg.setDefaultEncoding("UTF-8");
         cfg.setDirectoryForTemplateLoading(new File(ftlPath));
         cfg.setObjectWrapper(new DefaultObjectWrapper(Configuration.VERSION_2_3_29));
-        temp = cfg.getTemplate(ftlName);
+        temp = cfg.getTemplate(ftlName, "UTF-8");
+        temp.setEncoding("UTF-8");
     }
 
     public static void generator(String fileName, Map<String, Object> map) throws IOException, TemplateException {
         FileWriter fw = new FileWriter(fileName);
-        BufferedWriter bw = new BufferedWriter(fw);
+        FileOutputStream fileoutput = new FileOutputStream(new File(fileName));
+        OutputStreamWriter bw =  new OutputStreamWriter(fileoutput,"UTF-8");
         temp.process(map, bw);
         bw.flush();
         fw.close();
