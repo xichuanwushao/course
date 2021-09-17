2003470f9cc40016483c573d21217d3cedb85fd1
xichuanwushao <avatarwx@163.com>
2021-09-17 13:56:26 +0800

    5-9 前端增加接口调用日志 1.前端增加统一日志输出

--- admin/src/main.js
+++ admin/src/main.js
@@ -11,3 +11,15 @@ new Vue({
   router,
   render: h => h(App),
 }).$mount('#app')
+
+/**
+ * axios拦截器
+ */
+axios.interceptors.request.use(function (config) {
+  console.log("请求：", config);
+  return config;
+}, error => {});
+axios.interceptors.response.use(function (response) {
+  console.log("返回结果：", response);
+  return response;
+}, error => {});
