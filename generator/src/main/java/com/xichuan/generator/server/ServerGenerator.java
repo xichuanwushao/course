77b94cbb09b3c8dbf91712fa7bcb024721e7faa0
xichuanwushao <avatarwx@163.com>
2021-09-18 13:11:32 +0800

    6-2 代码生成器开发 增加req代码生成方法

--- generator/src/main/java/com/xichuan/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/xichuan/generator/server/ServerGenerator.java
@@ -1,13 +1,15 @@
 package com.xichuan.generator.server;
 
 
+import com.xichuan.generator.util.DbUtil;
+import com.xichuan.generator.util.Field;
 import com.xichuan.generator.util.FreemarkerUtil;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class ServerGenerator {
     static String MODULE = "business";
+    static String toDtoPath = "server\\src\\main\\java\\com\\xichuan\\server\\req\\";
     static String toServicePath = "server\\src\\main\\java\\com\\xichuan\\server\\service\\";
     static String toControllerPath = MODULE+"\\src\\main\\java\\com\\xichuan\\"+MODULE+"\\controller\\";
     public static void main(String[] args) throws Exception {
@@ -15,17 +17,35 @@ public class ServerGenerator {
         String domain = "section";
         String tableNameCn = "小节";
         String module = MODULE;
-        Map<String,Object> map = new HashMap<>();
+        List<Field> fieldList = DbUtil.getColumnByTableName(domain);
+        Set<String> typeSet = getJavaTypes(fieldList);
+        Map<String, Object> map = new HashMap<>();
         map.put("Domain",Domain);
         map.put("domain",domain);
         map.put("tableNameCn",tableNameCn);
         map.put("module",module);
+        map.put("fieldList", fieldList);
+        map.put("typeSet", typeSet);
         FreemarkerUtil.initConfig("service.ftl");
         FreemarkerUtil.generator(toServicePath+Domain+"Service.java",map);
 
         FreemarkerUtil.initConfig("controller.ftl");
         FreemarkerUtil.generator(toControllerPath+Domain+"Controller.java",map);
+        // 生成dto
+        FreemarkerUtil.initConfig("req.ftl");
+        FreemarkerUtil.generator(toDtoPath + Domain + "Req.java", map);
+
     }
 
-
+    /**
+     * 获取所有的Java类型，使用Set去重
+     */
+    private static Set<String> getJavaTypes(List<Field> fieldList) {
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < fieldList.size(); i++) {
+            Field field = fieldList.get(i);
+            set.add(field.getJavaType());
+        }
+        return set;
+    }
 }
