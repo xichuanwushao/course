ccc21a13c5a843a9bc513ac1cde2738e2b0490b7
xichuanwushao <avatarwx@163.com>
2021-09-20 15:53:55 +0800

    6-7 course模块前端代码使用VueGenerator代码生成器生成

---
+++ admin/src/views/admin/course.vue
@@ -0,0 +1,309 @@
+<template xmlns:v-on="http://www.w3.org/1999/xhtml">
+    <div>
+        <!--        <button v-on:click="list(1)" id="Loading-btn" type="button" class="btn btn-success" data-Loading-text="Loading..."><i class="ace-icon fa fa-refresh "></i><font class="Loading-font">刷新</font></button>-->
+        <button v-on:click="add()" class="btn btn-white btn-default btn-round">
+            <i class="ace-icon fa fa-edit "></i>
+            新增
+        </button>
+        <button v-on:click="list(1)" class="btn btn-white btn-default btn-round">
+            <i class="ace-icon fa fa-refresh "></i>
+            刷新
+        </button>
+        <!-- PAGE CONTENT BEGINS -->
+        <pagination  ref="pagination" v-bind:list="list" v-bind:itemCount="3"></pagination>
+        <table id="simple-table" class="table  table-bordered table-hover">
+            <thead>
+            <tr>
+            <th>id</th>
+            <th>名称</th>
+            <th>概述</th>
+            <th>时长</th>
+            <th>价格(元)</th>
+            <th>封面</th>
+            <th>级别</th>
+            <th>收费</th>
+            <th>状态</th>
+            <th>报名数</th>
+            <th>顺序</th>
+            <th>操作</th>
+            <tr/>
+        </thead>
+
+        <tbody>
+        <tr v-for="course in courses">
+            <td>{{course.id}}</td>
+            <td>{{course.name}}</td>
+            <td>{{course.summary}}</td>
+            <td>{{course.time}}</td>
+            <td>{{course.price}}</td>
+            <td>{{course.image}}</td>
+            <td>{{course.level}}</td>
+            <td>{{course.charge}}</td>
+            <td>{{course.status}}</td>
+            <td>{{course.enroll}}</td>
+            <td>{{course.sort}}</td>
+        <td>
+            <div class="hidden-sm hidden-xs btn-group">
+
+                <button v-on:click="edit(course)" class="btn btn-xs btn-info">
+                    <i class="ace-icon fa fa-pencil bigger-120"></i>
+                </button>
+
+                <button v-on:click="del(course.id)" class="btn btn-xs btn-danger">
+                    <i class="ace-icon fa fa-trash-o bigger-120"></i>
+                </button>
+
+            </div>
+
+            <div class="hidden-md hidden-lg">
+                <div class="inline pos-rel">
+                    <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown" data-position="auto">
+                        <i class="ace-icon fa fa-cog icon-only bigger-110"></i>
+                    </button>
+
+                    <ul class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close">
+                        <li>
+                            <a href="#" class="tooltip-info" data-rel="tooltip" title="View">
+                                                                    <span class="blue">
+                                                                        <i class="ace-icon fa fa-search-plus bigger-120"></i>
+                                                                    </span>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
+                                                                    <span class="green">
+                                                                        <i class="ace-icon fa fa-pencil-square-o bigger-120"></i>
+                                                                    </span>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
+                                                                    <span class="red">
+                                                                        <i class="ace-icon fa fa-trash-o bigger-120"></i>
+                                                                    </span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </td>
+        </tr>
+        </tbody>
+        </table>
+        <!-- PAGE CONTENT ENDS -->
+
+        <div id="form-modal" class="modal fade" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title">表单</h4>
+                    </div>
+                    <div class="modal-body">
+                        <form class="form-horizontal">
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">名称</label>
+                                <div class="col-sm-10">
+                                    <input v-model="course.name" class="form-control">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">概述</label>
+                                <div class="col-sm-10">
+                                    <input v-model="course.summary" class="form-control">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">时长</label>
+                                <div class="col-sm-10">
+                                    <input v-model="course.time" class="form-control">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">价格(元)</label>
+                                <div class="col-sm-10">
+                                    <input v-model="course.price" class="form-control">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">封面</label>
+                                <div class="col-sm-10">
+                                    <input v-model="course.image" class="form-control">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">级别</label>
+                                <div class="col-sm-10">
+                                    <input v-model="course.level" class="form-control">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">收费</label>
+                                <div class="col-sm-10">
+                                    <input v-model="course.charge" class="form-control">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">状态</label>
+                                <div class="col-sm-10">
+                                    <input v-model="course.status" class="form-control">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">报名数</label>
+                                <div class="col-sm-10">
+                                    <input v-model="course.enroll" class="form-control">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label">顺序</label>
+                                <div class="col-sm-10">
+                                    <input v-model="course.sort" class="form-control">
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
+                        <button v-on:click="save()" type="button" class="btn btn-primary">保存</button>
+                    </div>
+                </div><!-- /.modal-content -->
+            </div><!-- /.modal-dialog -->
+        </div><!-- /.modal -->
+    </div>
+</template>
+<script>
+    import Pagination from "../../components/pagination";
+    import Swal from 'sweetalert2'
+    export default {
+        components: {Pagination},
+        name: "course",
+        data:function (){
+            return{
+            course:{},
+            courses:[],
+                currentPage:{},
+        }
+        },
+        mounted:function () {
+            // this.$parent.activeSidebar("business-course-sidebar");
+            let _this = this;
+            _this.$refs.pagination.size = 5;
+            _this.list(1);
+        },
+        methods:{
+            add(){
+                let _this = this;
+                _this.course={};
+                $("#form-modal").modal("show")
+            },
+
+            edit(course){
+                let _this = this;
+                _this.course = $.extend({},course);//对象复制
+                $("#form-modal").modal("show")
+            },
+            list(page){
+                let _this = this;
+                Loading.show();
+                _this.currentPage=page,
+                    _this.$ajax.post(process.env.VUE_APP_SERVER+"/business/course/listPage",{
+                        page:page,
+                        size:_this.$refs.pagination.size,
+                    }).then((response=>{
+                        Loading.hide();
+                        // console.log("查询章列表结果：",response);
+                        let resp = response.data;
+                        _this.courses = resp.content.list;
+                        _this.$refs.pagination.render(page, resp.content.total);
+                    }))
+            },
+            save(){
+                let _this = this;
+                // 保存校验 TODO
+                if (1 != 1
+                    || !Validator.require(_this.course.name, "名称")
+                    || !Validator.length(_this.course.name, "名称", 1, 50)
+                    || !Validator.length(_this.course.summary, "概述", 1, 2000)
+                    || !Validator.require(_this.course.price, "价格(元)")
+                    || !Validator.length(_this.course.image, "封面", 1, 100)
+                    || !Validator.require(_this.course.level, "级别")
+                ) {
+                    return;
+                }
+
+                Loading.show();
+                _this.$ajax.post(process.env.VUE_APP_SERVER+"/business/course/save", _this.course).then((response=>{
+                    Loading.hide();
+                    // console.log("保存章列表结果：",response);
+                    let resp = response.data;
+                    if (resp.success){
+                        $("#form-modal").modal("hide");
+                        _this.list(1);
+                        toast.success("保存成功")
+                    }else{
+                        toast.success(resp.message)
+                    }
+                }))
+            },
+            del(id){
+                let _this = this;
+                Confirm.show("删除章节不可恢复确认删除?",function (){
+                    Loading.show();
+                    _this.$ajax.delete(process.env.VUE_APP_SERVER+"/business/course/delete/"+id, _this.course).then((response=>{
+                        Loading.hide();
+                        // console.log("删除课程列表结果：",response);
+                        let resp = response.data;
+                        if (resp.success){
+                            _this.list( _this.currentPage);
+                            toast.success("删除成功")
+                        }
+                    }))
+                })
+                // Swal.fire({
+                //     title: '确认删除?',
+                //     text: "删除不可恢复确认删除?",
+                //     icon: 'warning',
+                //     showCancelButton: true,
+                //     confirmButtonColor: '#3085d6',
+                //     cancelButtonColor: '#d33',
+                //     confirmButtonText: '确认!'
+                // }).then((result) => {
+                //     if (result.isConfirmed) {
+                //         Loading.show();
+                //         _this.$ajax.delete("http://127.0.0.1:9000/business/course/delete/"+id, _this.course).then((response=>{
+                //             Loading.hide();
+                //             console.log("删除课程列表结果：",response);
+                //             let resp = response.data;
+                //             if (resp.success){
+                //                 _this.list( _this.currentPage);
+                //                 toast.success("删除成功")
+                //             }
+                //         }))
+                //     }
+                // });
+
+            }
+        }
+
+    }
+</script>
+<style>
+    .btn{
+        margin-top: 0;
+        margin-right: 5px;
+    }
+    #Loading-btn{
+        width: 120px;
+        height: 20px;
+        margin-bottom: 6px;
+        font-size: 14px;
+        font-margin-bottom: 6px;
+        padding-bottom: 25px;
+    }
+    .Loading-font{
+        /*padding-bottom: 12px;*/
+    }
+</style>
