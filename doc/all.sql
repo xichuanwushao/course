674a08c262b63f9da3afacfcceac7101d6fb7bf0
xichuanwushao <avatarwx@163.com>
2021-09-20 15:49:16 +0800

    6-7 course模块持久层表创建 枚举类创建

--- doc/all.sql
+++ doc/all.sql
@@ -77,3 +77,34 @@ INSERT INTO `section` (id, title, course_id, chapter_id, video, time, charge, so
 VALUES ('10','反贪风暴','1','0','','500','F','1',null,null);
 INSERT INTO `section` (id, title, course_id, chapter_id, video, time, charge, sort, created_at, updated_at)
 VALUES ('11','流浪地球','1','0','','500','F','1',null,null);
+
+drop table if exists course;
+create table course (
+    id char(8) not null default '' comment 'id',
+    name varchar(50) not null  comment '名称',
+    summary varchar(2000)  comment '概述',
+    time int default 0  comment '时长|单位秒',
+    price decimal(8,2) not null default 0.00 comment '价格(元)',
+    image varchar(100) comment '封面',
+    level char(1) not null comment '级别|ONE("1","初级"),TWO("2","中级"),THREE("3","高级")',
+    charge char(1)  comment '收费|CHARGE("C","收费"),FREE("F","免费")',
+    status char(1)  comment '状态|PUBLISH("P","发布"),DRAFT("D","草稿")',
+    enroll integer default 0 comment '报名数',
+    sort int comment '顺序',
+    created_at datetime(3) comment '创建时间',
+    updated_at datetime(3) comment '修改时间',
+    primary key (id)
+)  ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='课程';
+
+insert into course (id, name, summary, time, price, image, level, charge, status, enroll, sort, created_at, updated_at) VALUES ('1','钢铁侠','电影获得奥斯卡奖项,年度最佳纸片人',7200,19.8,'',0,'C','D',100,0,now(),now());
+insert into course (id, name, summary, time, price, image, level, charge, status, enroll, sort, created_at, updated_at) VALUES ('2','变形金刚5','电影获得奥斯卡奖项,年度最佳纸片人',7200,19.8,'',0,'C','D',100,0,now(),now());
+insert into course (id, name, summary, time, price, image, level, charge, status, enroll, sort, created_at, updated_at) VALUES ('3','阿凡达3','电影获得奥斯卡奖项,年度最佳纸片人',7200,19.8,'',0,'C','D',100,0,now(),now());
+insert into course (id, name, summary, time, price, image, level, charge, status, enroll, sort, created_at, updated_at) VALUES ('4','大黄蜂','电影获得奥斯卡奖项,年度最佳纸片人',7200,19.8,'',0,'C','D',100,0,now(),now());
+insert into course (id, name, summary, time, price, image, level, charge, status, enroll, sort, created_at, updated_at) VALUES ('5','绿巨人','电影获得奥斯卡奖项,年度最佳纸片人',7200,19.8,'',0,'C','D',100,0,now(),now());
+insert into course (id, name, summary, time, price, image, level, charge, status, enroll, sort, created_at, updated_at) VALUES ('6','血战钢锯岭','电影获得奥斯卡奖项,年度最佳纸片人',7200,19.8,'',0,'C','D',100,0,now(),now());
+insert into course (id, name, summary, time, price, image, level, charge, status, enroll, sort, created_at, updated_at) VALUES ('7','战狼3','电影获得奥斯卡奖项,年度最佳纸片人',7200,19.8,'',0,'C','D',100,0,now(),now());
+insert into course (id, name, summary, time, price, image, level, charge, status, enroll, sort, created_at, updated_at) VALUES ('8','泰囧','电影获得奥斯卡奖项,年度最佳纸片人',7200,19.8,'',0,'C','D',100,0,now(),now());
+insert into course (id, name, summary, time, price, image, level, charge, status, enroll, sort, created_at, updated_at) VALUES ('9','我和我的祖国','电影获得奥斯卡奖项,年度最佳纸片人',7200,19.8,'',0,'C','D',100,0,now(),now());
+insert into course (id, name, summary, time, price, image, level, charge, status, enroll, sort, created_at, updated_at) VALUES ('10','反贪风暴','电影获得奥斯卡奖项,年度最佳纸片人',7200,19.8,'',0,'C','D',100,0,now(),now());
+insert into course (id, name, summary, time, price, image, level, charge, status, enroll, sort, created_at, updated_at) VALUES ('11','流浪地球','电影获得奥斯卡奖项,年度最佳纸片人',7200,19.8,'',0,'C','D',100,0,now(),now());
+insert into course (id, name, summary, time, price, image, level, charge, status, enroll, sort, created_at, updated_at) VALUES ('12','刺客信条','电影获得奥斯卡奖项,年度最佳纸片人',7200,19.8,'',0,'C','D',100,0,now(),now());
