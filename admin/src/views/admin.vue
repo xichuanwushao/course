7b7a87fa8bfaef26616cbb92a96d700ad15aea71
xichuanwushao <avatarwx@163.com>
2021-09-27 15:05:35 +0800

    7-3 category基本代码生成 测试页面正常

--- admin/src/views/admin.vue
+++ admin/src/views/admin.vue
@@ -474,6 +474,17 @@
                         <b class="arrow"></b>
 
                         <ul class="submenu">
+                            <li class="" id="business-category-sidebar">
+                                <router-link to="/business/category" class="dropdown-toggle">
+                                    <i class="menu-icon fa fa-caret-right"></i>
+
+                                    分类管理
+                                    <b class="arrow fa fa-angle-down"></b>
+                                </router-link>
+
+                                <b class="arrow"></b>
+
+                            </li>
                             <li class="" id="business-course-sidebar">
                                 <router-link to="/business/course" class="dropdown-toggle">
                                     <i class="menu-icon fa fa-caret-right"></i>
