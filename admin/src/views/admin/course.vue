b7a58d11eb8d84d9ec8adebc28a80df7d9fb867c
xichuanwushao <avatarwx@163.com>
2021-09-28 14:49:35 +0800

    7-4 页面ztree组件对接真实数据 1.保存课程时 保存所有分类 2.参考ztree示例 admin/public/zTree_v3/demo/cn/excheck/checkbox.html

--- admin/src/views/admin/course.vue
+++ admin/src/views/admin/course.vue
@@ -238,13 +238,14 @@
                 COURSE_LEVEL: COURSE_LEVEL,
                 COURSE_CHARGE: COURSE_CHARGE,
                 COURSE_STATUS: COURSE_STATUS,
+                categorys:[],
             }
         },
         mounted:function () {
             // this.$parent.activeSidebar("business-course-sidebar");
             let _this = this;
             _this.$refs.pagination.size = 5;
-            _this.initTree();
+            _this.allCategory();
             _this.list(1);
         },
         methods:{
@@ -350,33 +351,35 @@
                 SessionStorage.set("course", course);
                 _this.$router.push("/business/chapter");//跳转到大章
             },
+            allCategory(){
+                let _this = this;
+                Loading.show();
+                _this.$ajax.post(process.env.VUE_APP_SERVER+"/business/category/all",{
+                }).then((response=>{
+                    Loading.hide();
+                    // console.log("查询章列表结果：",response);
+                    let resp = response.data;
+                    _this.categorys = resp.content;
+                    _this.initTree();
+                }))
+            },
             initTree() {
+                let _this = this;
                 let setting = {
                     check: {
                         enable: true
                     },
                     data: {
                         simpleData: {
-                            enable: true
+                            enable: true,
+                            idKey: "id",
+                            pIdKey: "parent",
+                            rootPId: "0",
                         }
                     }
                 };
 
-                let zNodes =[
-                    { id:1, pId:0, name:"随意勾选 1", open:true},
-                    { id:11, pId:1, name:"随意勾选 1-1", open:true},
-                    { id:111, pId:11, name:"随意勾选 1-1-1"},
-                    { id:112, pId:11, name:"随意勾选 1-1-2"},
-                    { id:12, pId:1, name:"随意勾选 1-2", open:true},
-                    { id:121, pId:12, name:"随意勾选 1-2-1"},
-                    { id:122, pId:12, name:"随意勾选 1-2-2"},
-                    { id:2, pId:0, name:"随意勾选 2", checked:true, open:true},
-                    { id:21, pId:2, name:"随意勾选 2-1"},
-                    { id:22, pId:2, name:"随意勾选 2-2", open:true},
-                    { id:221, pId:22, name:"随意勾选 2-2-1", checked:true},
-                    { id:222, pId:22, name:"随意勾选 2-2-2"},
-                    { id:23, pId:2, name:"随意勾选 2-3"}
-                ];
+                let zNodes = _this.categorys;
                 $.fn.zTree.init($("#tree"), setting, zNodes);
             },
         }
