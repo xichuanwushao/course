e28e3d4bfd5b899c46cc4770f06f2415360f3765
xichuanwushao <avatarwx@163.com>
2021-10-02 12:13:50 +0800

    7-7 teacher页面引用ace的profile.html样式 1.位置admin/public/ace/profile.html

--- admin/src/views/admin/teacher.vue
+++ admin/src/views/admin/teacher.vue
@@ -13,7 +13,111 @@
      </p>
         <!-- PAGE CONTENT BEGINS -->
         <pagination  ref="pagination" v-bind:list="list" v-bind:itemCount="3"></pagination>
-        <table id="simple-table" class="table  table-bordered table-hover">
+        <div class="col-xs-12 col-sm-3 center">
+            <div>
+                <span class="profile-picture">
+                    <img id="avatar" class="editable img-responsive" alt="Alex's Avatar" src="assets/images/avatars/profile-pic.jpg" />
+                </span>
+                <div class="space-4"></div>
+
+                <div class="width-80 label label-info label-xlg arrowed-in arrowed-in-right">
+                    <div class="inline position-relative">
+                        <a href="#" class="user-title-label dropdown-toggle" data-toggle="dropdown">
+                            <i class="ace-icon fa fa-circle light-green"></i>
+                            &nbsp;
+                            <span class="white">Alex M. Doe</span>
+                        </a>
+
+                        <ul class="align-left dropdown-menu dropdown-caret dropdown-lighter">
+                            <li class="dropdown-header"> Change Status </li>
+
+                            <li>
+                                <a href="#">
+                                    <i class="ace-icon fa fa-circle green"></i>
+&nbsp;
+                                    <span class="green">Available</span>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="#">
+                                    <i class="ace-icon fa fa-circle red"></i>
+&nbsp;
+                                    <span class="red">Busy</span>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="#">
+                                    <i class="ace-icon fa fa-circle grey"></i>
+&nbsp;
+                                    <span class="grey">Invisible</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="space-6"></div>
+
+            <div class="profile-contact-info">
+                <div class="profile-contact-links align-left">
+                    <a href="#" class="btn btn-link">
+                        <i class="ace-icon fa fa-plus-circle bigger-120 green"></i>
+                        Add as a friend
+                    </a>
+
+                    <a href="#" class="btn btn-link">
+                        <i class="ace-icon fa fa-envelope bigger-120 pink"></i>
+                        Send a message
+                    </a>
+
+                    <a href="#" class="btn btn-link">
+                        <i class="ace-icon fa fa-globe bigger-125 blue"></i>
+                        www.alexdoe.com
+                    </a>
+                </div>
+
+                <div class="space-6"></div>
+
+                <div class="profile-social-links align-center">
+                    <a href="#" class="tooltip-info" title="" data-original-title="Visit my Facebook">
+                        <i class="middle ace-icon fa fa-facebook-square fa-2x blue"></i>
+                    </a>
+
+                    <a href="#" class="tooltip-info" title="" data-original-title="Visit my Twitter">
+                        <i class="middle ace-icon fa fa-twitter-square fa-2x light-blue"></i>
+                    </a>
+
+                    <a href="#" class="tooltip-error" title="" data-original-title="Visit my Pinterest">
+                        <i class="middle ace-icon fa fa-pinterest-square fa-2x red"></i>
+                    </a>
+                </div>
+            </div>
+
+            <div class="hr hr12 dotted"></div>
+
+            <div class="clearfix">
+                <div class="grid2">
+                    <span class="bigger-175 blue">25</span>
+
+                    <br />
+                    Followers
+                </div>
+
+                <div class="grid2">
+                    <span class="bigger-175 blue">12</span>
+
+                    <br />
+                    Following
+                </div>
+            </div>
+
+            <div class="hr hr16 dotted"></div>
+        </div>
+
+      <table id="simple-table" class="table  table-bordered table-hover">
             <thead>
             <tr>
             <th>id</th>
