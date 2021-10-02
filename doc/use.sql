579147436c6c7e5ab63b25afbaff43757a70f48e
xichuanwushao <avatarwx@163.com>
2021-10-02 14:04:13 +0800

    7-7 课程和讲师表关联 给course新增字段teacher_id 重新生成持久层代码 1.课程和分类属于多对多的关系 课程和讲师属于一对多的关系

--- doc/use.sql
+++ doc/use.sql
@@ -2,3 +2,5 @@
 
 update course c set `time` = (select sum(`time`) from `section` where course_id = #{courseId})
 where c.id = #{courseId}
+
+alter table `course` add column (`teacher_id` char(8) comment `讲师|teacher.id`);
