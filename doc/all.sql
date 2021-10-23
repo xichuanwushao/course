666fc096eea5a2fb8fe992dc5bfc4119dad7d28e
xichuanwushao <avatarwx@163.com>
2021-10-23 17:11:18 +0800

    11-1 增加用户管理功能:增加用户表 自动生成持久层代码

--- doc/all.sql
+++ doc/all.sql
@@ -212,3 +212,15 @@ create table `course_content_file` (
     primary key (`id`)
 ) engine=innodb default charset=utf8mb4 comment='课程文件内容';
 
+
+drop table if exists `user`;
+create table `user` (
+    `id` char(8) not null default '' comment 'id',
+    `login_name` varchar(50) not null  comment '登录名',
+    `name` varchar(50)  comment '昵称',
+    `password` char(32) not null  comment '密码',
+    primary key (`id`),
+    unique key `login_name_unique` (`login_name`)
+) engine=innodb default charset=utf8mb4 comment='用户';
+
+insert into `user` (id, login_name, name, password) values ('100000000','test','测试','asda54da4da4d65as4dasd5a4sd54as6');
