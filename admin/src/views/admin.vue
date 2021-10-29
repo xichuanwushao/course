7dcf75cfaebcab313d3bbf6db7cb176a5955635f
xichuanwushao <avatarwx@163.com>
2021-10-29 20:23:03 +0800

    12-5 权限拦截功能开发 增加路由权限的判断 1.输入http://localhost:8080/system/resource不会跳转权限管理界面，而是跳转首页

--- admin/src/views/admin.vue
+++ admin/src/views/admin.vue
@@ -646,6 +646,11 @@
             $("body").attr("class", "no-skin");
             _this.activeSidebar(_this.$route.name.replace("/", "-") + "-sidebar");
             _this.loginUser = Tool.getLoginUser();
+
+
+            if (!_this.hasResourceRouter(_this.$route.name)) {
+                _this.$router.push("/login");
+            }
         },
         watch: {
             $route: {
@@ -654,6 +659,11 @@
                     console.log("---->页面跳转：", val, oldVal);
                     let _this = this;
 
+                    if (!_this.hasResourceRouter(val.name)) {
+                        _this.$router.push("/login");
+                        return;
+                    }
+
                     _this.$nextTick(function(){  //页面加载完成后执行
                         _this.activeSidebar(_this.$route.name.replace("/", "-") + "-sidebar");
                     })
@@ -661,6 +671,23 @@
             }
         },
         methods:{
+            /**
+             * 查找是否有权限
+             * @param router
+             */
+            hasResourceRouter(router) {
+                let _this = this;
+                let resources = Tool.getLoginUser().resources;
+                if (Tool.isEmpty(resources)) {
+                    return false;
+                }
+                for (let i = 0; i < resources.length; i++) {
+                    if (router === resources[i].page) {
+                        return true;
+                    }
+                }
+                return false;
+            },
             /**
              * 查找是否有权限
              * @param id
