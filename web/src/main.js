b82cacd6cc50e839b0777df5042122c9bceb9449
xichuanwushao <avatarwx@163.com>
2021-10-30 18:11:09 +0800

    网站首页开发 增加路由配置router.js 增加网站首页index.vue

--- web/src/main.js
+++ web/src/main.js
@@ -1,10 +1,12 @@
-import Vue from 'vue'
-import App from './app.vue'
 import AppBlog from './App-Blog.vue'
 import AppCarousel from './App-Carousel.vue'
+import Vue from 'vue'
+import App from './app.vue'
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
