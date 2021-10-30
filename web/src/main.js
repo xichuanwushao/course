2c39297ed9fedf63e9962f0f9fdc7cc94324fed7
xichuanwushao <avatarwx@163.com>
2021-10-30 15:21:24 +0800

    13-1 网站首页开发 集成blog模板 1.https://v4.bootcss.com/docs/examples/blog/

--- web/src/main.js
+++ web/src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import AppBlog from './App-Blog.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(AppBlog),
 }).$mount('#app')
