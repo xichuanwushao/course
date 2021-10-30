1341a3d12383119bc713268c5284e4e0da4b306d
xichuanwushao <avatarwx@163.com>
2021-10-30 16:01:36 +0800

    13-1 网站首页开发 集成carousel模板 1.https://getbootstrap.com/docs/4.4/examples/carousel/

--- web/src/main.js
+++ web/src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import AppBlog from './App-Blog.vue'
+import AppCarousel from './App-Carousel.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(AppBlog),
+  render: h => h(AppCarousel),
 }).$mount('#app')
