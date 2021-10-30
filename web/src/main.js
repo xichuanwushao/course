01a65cdee19f86553bbc1fb455f58f2af1b80d44
xichuanwushao <avatarwx@163.com>
2021-10-30 17:08:14 +0800

    13-2 网站首页开发 将顶部提取成the-header组件

--- web/src/main.js
+++ web/src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './app.vue'
 import AppBlog from './App-Blog.vue'
 import AppCarousel from './App-Carousel.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(AppCarousel),
+  render: h => h(App),
 }).$mount('#app')
