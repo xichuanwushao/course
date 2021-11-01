fa2b077fbf9c8f20e0a7983a8da10967a227f8d0
xichuanwushao <avatarwx@163.com>
2021-11-01 20:14:27 +0800

    13-6 课程列表页面开发 点击一级分类时 显示激活状态 并显示对应的二级分类 如果点击的是【全部】 则显示所有的二级分类

--- web/src/views/list.vue
+++ web/src/views/list.vue
@@ -55,6 +55,7 @@
         courses: [],
           level1:[],
           level2:[],
+          categorys:[],
       }
     },
     mounted() {
@@ -99,15 +100,6 @@
                     let c = _this.categorys[i];
                     if (c.parent === '0') {
                         _this.level1.push(c);
-                        for (let j = 0; j < _this.categorys.length; j++) {
-                            let child = _this.categorys[j];
-                            if (child.parent === c.id) {
-                                if (Tool.isEmpty(c.children)) {
-                                    c.children = [];
-                                }
-                                c.children.push(child);
-                            }
-                        }
                     }else{
                         _this.level2.push(c);
                     }
@@ -120,6 +112,38 @@
          */
         onClickLevel1(level1Id) {
             let _this = this;
+            // 点击一级分类时，显示激活状态
+            $("#category-" + level1Id).siblings("a").removeClass("cur");
+            $("#category-" + level1Id).addClass("cur");
+
+            // 点击一级分类时，二级分类【无限】按钮要设置激活状态
+            $("#category-1").siblings("a").removeClass("on");
+            $("#category-1").addClass("on");
+
+            // 注意：要先把level2中所有的值清空，再往里放
+            _this.level2 = [];
+            let categorys = _this.categorys;
+            // 如果点击的是【全部】，则显示所有的二级分类
+            if (level1Id === '0') {
+                for (let i = 0; i < categorys.length; i++) {
+                    let c = categorys[i];
+                    if (c.parent !== "0") {
+                        _this.level2.push(c);
+                    }
+                }
+            }
+            // 如果点击的是某个一级分类，则显示该一级分类下的二级分类
+            if (level1Id !== '0') {
+                for (let i = 0; i < categorys.length; i++) {
+                    let c = categorys[i];
+                    if (c.parent === level1Id) {
+                        _this.level2.push(c);
+                    }
+                }
+            }
+
+            // 重新加载课程列表
+            _this.listCourse(1);
         },
 
         /**
