3003acea46e2ca49f6437ea4c2adf8d06e527ae0
xichuanwushao <avatarwx@163.com>
2021-11-03 19:55:42 +0800

    13-8 课程详情页面开发 使用本地视频链接进行播放 1.使用video标签

--- web/src/views/detail.vue
+++ web/src/views/detail.vue
@@ -58,7 +58,7 @@
                         <tr v-for="(s, j) in chapter.sections" class="chapter-section-tr">
                           <td class="col-sm-8 col-xs-12">
                             <div  v-on:click="play(s)" class="section-title">
-<!--                              <video v-bind:src="s.video" controls="controls" id="videoss"  ></video>-->
+                              <video v-bind:src="s.video" controls="controls" id="videoss"  ></video>
                               <i class="fa fa-video-camera d-none d-sm-inline"></i>&nbsp;&nbsp;
                               <span class="d-none d-sm-inline">第{{j+1}}节&nbsp;&nbsp;</span>
                               {{s.title}}
@@ -161,9 +161,9 @@ export default {
     play(section) {
       let _this = this;
       // _this.$refs.modalPlayer.playUrl(section.video);
-        _this.$refs.player.playUrl(section.video);
-        console.info("***"+section.vod)
-     // _this.$refs.modalPlayer.playVod(section.vod);
+      // _this.$refs.player.playUrl(section.video);
+          console.info("***"+section.vod)
+      // _this.$refs.modalPlayer.playVod(section.vod);
     },
   }
 }
