29d72bbc43ecfc783b9b1897e988646c6290ed2f
xichuanwushao <avatarwx@163.com>
2021-11-03 18:41:51 +0800

    13-8 课程详情页面开发 视频播放功能 1.引入所需文件从admin

---
+++ web/public/static/js/toast.js
@@ -0,0 +1,44 @@
+// const Toast = Swal.mixin({
+//     toast: true,
+//     position: 'top-end',
+//     showConfirmButton: false,
+//     timer: 3000,
+//     timerProgressBar: true,
+//     didOpen: (toast) => {
+//         toast.addEventListener('mouseenter', Swal.stopTimer)
+//         toast.addEventListener('mouseleave', Swal.resumeTimer)
+//     }
+// })
+
+
+
+let toast;
+toast = {
+    success:function (message) {
+        Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: message,
+            showConfirmButton: false,
+            timer: 3000
+        })
+    },
+    error:function (message) {
+        Swal.fire({
+            position: 'top-end',
+            icon: 'error',
+            title: message,
+            showConfirmButton: false,
+            timer: 3000
+        })
+    },
+    warning:function (message) {
+        Swal.fire({
+            position: 'top-end',
+            icon: 'warning',
+            title: message,
+            showConfirmButton: false,
+            timer: 3000
+        })
+    }
+}
