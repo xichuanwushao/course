834dcd07e0948b76a8122bdc3f5237f9f8e2d753
xichuanwushao <avatarwx@163.com>
2021-11-15 10:43:50 +0800

    14-2 控台欢迎页开发 5个方格数据展示 1.参考样例admin/public/ace/profile.html

--- admin/src/views/admin/welcome.vue
+++ admin/src/views/admin/welcome.vue
@@ -1,6 +1,56 @@
 <template>
     <div>
-        <h1>您好 欢迎进入控制台</h1>
+        <div class="space-12"></div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-9">
+                    <div class="center">
+                        <span class="btn btn-app btn-sm btn-light no-hover">
+                            <span class="line-height-1 bigger-170 blue"> 1,411 </span>
+
+                            <br />
+                            <span class="line-height-1 smaller-90"> Views </span>
+                        </span>
+
+                        <span class="btn btn-app btn-sm btn-yellow no-hover">
+													<span class="line-height-1 bigger-170"> 32 </span>
+
+													<br />
+													<span class="line-height-1 smaller-90"> Followers </span>
+												</span>
+
+                        <span class="btn btn-app btn-sm btn-pink no-hover">
+													<span class="line-height-1 bigger-170"> 4 </span>
+
+													<br />
+													<span class="line-height-1 smaller-90"> Projects </span>
+												</span>
+
+                        <span class="btn btn-app btn-sm btn-grey no-hover">
+													<span class="line-height-1 bigger-170"> 23 </span>
+
+													<br />
+													<span class="line-height-1 smaller-90"> Reviews </span>
+												</span>
+
+                        <span class="btn btn-app btn-sm btn-success no-hover">
+													<span class="line-height-1 bigger-170"> 7 </span>
+
+													<br />
+													<span class="line-height-1 smaller-90"> Albums </span>
+												</span>
+
+                        <span class="btn btn-app btn-sm btn-primary no-hover">
+													<span class="line-height-1 bigger-170"> 55 </span>
+
+													<br />
+													<span class="line-height-1 smaller-90"> Contacts </span>
+												</span>
+                    </div>
+
+                    <div class="space-12"></div>
+                </div>
+
+            </div>
     </div>
 </template>
 <script>
