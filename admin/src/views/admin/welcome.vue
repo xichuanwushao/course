6a7c1a40f1ad79db17fa627c5e678792f01ff829
xichuanwushao <avatarwx@163.com>
2021-11-15 11:04:35 +0800

    14-2 控台欢迎页开发 增加3个数据图标 1.参考样例admin/public/ace/index.html

--- admin/src/views/admin/welcome.vue
+++ admin/src/views/admin/welcome.vue
@@ -53,7 +53,49 @@
                     <div class="space-12"></div>
 
                 </div>
+                <div class="col-sm-7 infobox-container">
+                    <div class="infobox infobox-green">
+                        <div class="infobox-icon">
+                            <i class="ace-icon fa fa-comments"></i>
+                        </div>
 
+                        <div class="infobox-data">
+                            <span class="infobox-data-number">32</span>
+                            <div class="infobox-content">comments + 2 reviews</div>
+                        </div>
+
+                        <div class="stat stat-success">8%</div>
+                    </div>
+
+                    <div class="infobox infobox-blue">
+                        <div class="infobox-icon">
+                            <i class="ace-icon fa fa-flask"></i>
+                        </div>
+
+                        <div class="infobox-data">
+                            <span class="infobox-data-number">11</span>
+                            <div class="infobox-content">new followers</div>
+                        </div>
+
+                        <div class="badge badge-success">
+                            +32%
+                            <i class="ace-icon fa fa-arrow-up"></i>
+                        </div>
+                    </div>
+
+                    <div class="infobox infobox-pink">
+                        <div class="infobox-icon">
+                            <i class="ace-icon fa fa-shopping-cart"></i>
+                        </div>
+
+                        <div class="infobox-data">
+                            <span class="infobox-data-number">8</span>
+                            <div class="infobox-content">new orders</div>
+                        </div>
+                        <div class="stat stat-important">4%</div>
+                    </div>
+
+                </div>
             </div>
     </div>
 </template>
