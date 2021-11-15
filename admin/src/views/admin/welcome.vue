a05984ec4d4da1da1545d5ebabaffe897426f0e9
xichuanwushao <avatarwx@163.com>
2021-11-15 11:28:01 +0800

    14-2 控台欢迎页开发 增加1个图表 1.参考样例admin/public/ace/index.html

--- admin/src/views/admin/welcome.vue
+++ admin/src/views/admin/welcome.vue
@@ -102,6 +102,28 @@
                     <div class="space-12"></div>
 
                 </div>
+                <div class="col-sm-7">
+                    <div class="widget-box transparent">
+                        <div class="widget-header widget-header-flat">
+                            <h4 class="widget-title lighter">
+                                <i class="ace-icon fa fa-signal"></i>
+                                Sale Stats
+                            </h4>
+
+                            <div class="widget-toolbar">
+                                <a href="#" data-action="collapse">
+                                    <i class="ace-icon fa fa-chevron-up"></i>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="widget-body">
+                            <div class="widget-main padding-4">
+                                <div id="sales-charts"></div>
+                            </div><!-- /.widget-main -->
+                        </div><!-- /.widget-body -->
+                    </div><!-- /.widget-box -->
+                </div><!-- /.col -->
             </div>
     </div>
 </template>
@@ -110,8 +132,52 @@
         name: "welcome",
         mounted:function () {
             // this.$parent.activeSidebar("welcome-sidebar");
+            let _this = this;
+            _this.drawSaleChart();
         },
         methods:{
+            drawSaleChart(){
+                // 生成随机两组数据
+                let d1 = [];
+                for (let i = 0; i < 30; i += 1) {
+                    d1.push([i + 1, 2000 + Math.floor((Math.random()*100)+1)]);
+                }
+                let d2 = [];
+                for (let i = 0; i < 30; i += 1) {
+                    d2.push([i + 1, 1900 + Math.floor((Math.random()*100)+1)]);
+                }
+                let d3 = [];
+                for (let i = 0; i < 30; i += 1) {
+                    d3.push([i + 1, 1900 + Math.floor((Math.random()*100)+1)]);
+                }
+                var sales_charts = $('#sales-charts').css({'width':'100%' , 'height':'220px'});
+                $.plot("#sales-charts", [
+                    { label: "Domains", data: d1 },
+                    { label: "Hosting", data: d2 },
+                    { label: "Services", data: d3 }
+                ], {
+                    hoverable: true,
+                    shadowSize: 0,
+                    series: {
+                        lines: { show: true },
+                        points: { show: true }
+                    },
+                    xaxis: {
+                        tickLength: 0
+                    },
+                    yaxis: {
+                        ticks: 10,
+                        min: -2,
+                        max: 2,
+                        tickDecimals: 3
+                    },
+                    grid: {
+                        backgroundColor: { colors: [ "#fff", "#fff" ] },
+                        borderWidth: 1,
+                        borderColor:'#555'
+                    }
+                });
+            }
 
         }
 
